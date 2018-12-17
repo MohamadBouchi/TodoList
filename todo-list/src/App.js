491c45c7277c6359436c052431fe5b79f0b026a3
mohamedsubhi <mohamedsubhibouchi@gmail.com>
2018-12-17 13:51:02 +0100

    addTodo form created

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Todos from './todos/todos';
+import AddTodo from './addTodo/addTodo';
 
 class App extends Component {
   state = {
@@ -17,11 +18,19 @@ class App extends Component {
       todos
     });
   };
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos
+    });
+  };
   render() {
     return (
       <div className="todo-app container">
         <h1 className='center blue-text'>Todo's</h1>
         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo addTodo={this.addTodo}/>
       </div>
     );
   }
